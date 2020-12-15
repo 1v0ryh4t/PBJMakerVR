fb07ffe6231f505110a05b02aa77021d535c55c8
Tanner Reed <Septem216@gmail.com>
2020-12-15 11:05:31 -0500

    Created initial project with SteamVR, added bread, knives, and jars

---
+++ PBJMakerVR/Assets/Scripts/Collidable.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Collidable : MonoBehaviour
+{
+    public GameObject specialObj;
+
+    public UnityEvent onTriggerEnter;
+    public UnityEvent onTriggerExit;
+    public UnityEvent onTriggerStay;
+    public UnityEvent onCollisionEnter;
+    public UnityEvent onCollisionStay;
+    public UnityEvent onCollisionExit;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (specialObj == null || other == specialObj.GetComponent<Collider>())
+            onTriggerEnter.Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (specialObj == null || other == specialObj.GetComponent<Collider>())
+            onTriggerStay.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (specialObj == null || other == specialObj.GetComponent<Collider>())
+            onTriggerExit.Invoke();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (specialObj == null || collision.collider == specialObj.GetComponent<Collider>())
+            onCollisionEnter.Invoke();
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (specialObj == null || collision.collider == specialObj.GetComponent<Collider>())
+            onCollisionStay.Invoke();
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (specialObj == null || collision.collider == specialObj.GetComponent<Collider>())
+            onCollisionExit.Invoke();
+    }
+}
---
+++ PBJMakerVR/Assets/Scripts/HintObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintObject : MonoBehaviour
+{
+    /**
+     * This script makes an object flash yellow and a text box pops up with a prompt. The flashing and text should disappear when picked up
+     * This script should be attached to the gameobject associated with it
+     * */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //should flash and display hint
+    public void ShowHint()
+    {
+
+    }
+
+    //should stop flash and hint
+    public void HideHint()
+    {
+
+    }
+}
